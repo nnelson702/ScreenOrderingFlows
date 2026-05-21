1281d850dbc596e9741459ff0d3c298918f351a2
nnelson702 <nnelson@skyecos.com>
2026-05-21 12:42:35 -0700

    Add staff dashboard filter enhancement script

---
+++ staff-dashboard-v4-1-filters.js
@@ -0,0 +1,3 @@
+// Staff Dashboard V4.1 filter/sort enhancement script placeholder.
+// This file is staged separately because staff.html may need a manual include tag.
+console.log('Staff Dashboard V4.1 filters ready to wire.');
